f4fdf58e16f296e9b69d5d0bed014966cf373ae4
Steven Rothenburger <s.rothenburger1@gmail.com>
2019-01-25 10:00:35 -0700

    added rank feature and added background dynamic

--- src/Components/ImageLinkForm/ImageLinkForm.js
+++ src/Components/ImageLinkForm/ImageLinkForm.js
@@ -5,7 +5,7 @@ const ImageLinkForm = ()=>{
     return(
         <div>
             <p className='f3'>
-                {'This magic brain will detect faces in your pictures'}
+                {'This app will detect faces in your pictures'}
             </p>
             <div className='center'>
             <div className=' form center pa4 br3 shadow-5'>
