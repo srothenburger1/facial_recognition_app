e02198abf88651825a1168ca96f40b2a5cd5e999
Steven Rothenburger <s.rothenburger1@gmail.com>
2019-01-29 18:19:33 -0700

    fixed some issues and made sign in process better

--- src/App.js
+++ src/App.js
@@ -36,7 +36,8 @@ class App extends Component {
       input: '',
       imageUrl: '',
       box: {},
-      route: 'signin'
+      route: 'signin',
+      isSignedIn: false
 
     }
   }
@@ -56,7 +57,6 @@ class App extends Component {
 
   displayFaceBox = (box)=>{
     this.setState({box: box});
-    console.log(box)
   }
 
   onInputChange = (event) => {
@@ -74,16 +74,22 @@ class App extends Component {
   }
 
   onRouteChange = (route)=> {
+    if(route === 'signout'){
+      this.setState({isSignedIn:false})
+    }else if (route === 'home'){
+      this.setState({isSignedIn:true})
+    }
     this.setState({route: route})
   }
 
   render() {
+    const { isSignedIn, imageUrl, route, box } = this.state;
     return (
       <div className="App">
       <Particles className='particles'
         params={particlesOptions} />
-      <Navigation onRouteChange={this.onRouteChange}/>
-      { this.state.route ==='home'
+      <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange}/>
+      { route ==='home'
       ?<div> 
       <Logo />
       <Rank  />
@@ -91,10 +97,10 @@ class App extends Component {
       onInputChange={this.onInputChange} 
       onButtonSubmit={this.onButtonSubmit}
       />
-      <FaceRecognition box={this.state.box}imageUrl={this.state.imageUrl}/>
+      <FaceRecognition box={box}imageUrl={imageUrl}/>
       </div> 
       :(
-        this.state.route === 'signin' 
+        route === 'signin' 
         ? <SignIn onRouteChange={this.onRouteChange} />
         : <Register onRouteChange={this.onRouteChange} />
       )
