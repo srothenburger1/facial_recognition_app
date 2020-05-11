5daefafbd75673ff45c4803196fe439721a7c49e
Steven Rothenburger <srothenburger1@gmail.com>
2020-05-11 08:30:55 -0600

    played with google sigin info

--- src/Components/SignIn/GoogleSignin.jsx
+++ src/Components/SignIn/GoogleSignin.jsx
@@ -2,8 +2,64 @@ import React, { Component } from 'react'
 import ReactDOM from 'react-dom';
 import { GoogleLogin, GoogleLogout } from 'react-google-login';
 
-
 const CLIENT_ID = '612399434984-3bqm9gges27tses3f8vtub8111th9818.apps.googleusercontent.com';
+
+/*
+{
+  Ca: google id 
+  Qt: {
+    Ad: user full name,
+    DU: user last name,
+    DW: user first name,
+    ZU: google id,
+
+  },
+  accessToken: string,
+  googleId: id ,
+  profileObj: {
+    email,
+    familyName,
+    givenName,
+    googleId,
+    imageUrl,
+    name
+  },
+  tc: {
+    access_token: same as before,
+    expires_at: number, // its hard to say what format this is
+    expires_in: number,
+    first_issued_at: number, // also hard to determine
+    id_token: really long string,
+    idpId: google,
+    login_hint: hashed mess,
+    scope: looks like a list of links for user info,
+    session_state: {
+      extraQueryParams: {
+        authUser: number
+      },
+      token_type: "Bearer"
+    }
+    tokenId: really long string
+  },
+  tokenObj: {
+    access_token: same as before,
+    expires_at: number, // its hard to say what format this is
+    expires_in: number,
+    first_issued_at: number, // also hard to determine
+    id_token: really long string,
+    idpId: google,
+    login_hint: hashed mess,
+    scope: looks like a list of links for user info,
+    session_state: {
+      extraQueryParams: {
+        authUser: number
+      },
+      token_type: "Bearer"
+    }
+    tokenId: really long string
+  }, 
+}
+*/
 
 
 class GoogleBtn extends Component {
@@ -11,7 +67,7 @@ class GoogleBtn extends Component {
     super(props);
 
     this.state = {
-      isLogined: false,
+      isLoggedIn: false,
       accessToken: ''
     };
 
@@ -22,18 +78,18 @@ class GoogleBtn extends Component {
   }
 
   login (response) {
-    // if(response.Zi.access_token){
-    //   this.setState(state => ({
-    //     isLogined: true,
-    //     accessToken: response.Zi.access_token
-    //   }));
-    // }
+    if(response.tc.access_token){
+      this.setState(state => ({
+        isLoggedIn: true,
+        accessToken: response.tc.access_token
+      }));
+    }
     console.log(response)
   }
 
   logout (response) {
     this.setState(state => ({
-      isLogined: false,
+      isLoggedIn: false,
       accessToken: ''
     }));
   }
